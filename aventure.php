56fed5ec41bafb0095adf437eb0d27d70d013530
Julie POULEUR <julie.pouleur@student.junia.com>
2026-05-20 11:10:10 +0200

    ajout verif_case

--- aventure.php
+++ aventure.php
@@ -239,7 +239,7 @@
                 cell.classList.add("case");
                 cell.classList.add(`${row}`);
                 cell.classList.add(`${col}`);
-                
+
                 if ((row === 0 && col === 0) || (row === 8 && col === 8)) {
                     cell.classList.add("fixed");
                 }
@@ -333,6 +333,57 @@
                 i++
             }
             return tab_count
+        }
+
+        function verif_case(){
+            const tab = document.querySelectorAll(".case")
+            let tab_2 = Array.from({ length: 9 }, i => Array.from({ length: 9 }, j => 0));
+            for (let i =0; i < 9; i++){
+                for(let j = 0; j<9; j++){
+                    tab_2[i][j] = tab[i*9 +j]
+                }
+            }
+            console.log(tab_2)
+            let x = tab_2[0][0].classList
+            console.log(tab_2[0][0].classList)
+            let prev_1 = (0, 0)
+            let prev_2 = (0, 0)
+            for (let i =0; i < 9; i++){
+                for(let j = 0; j<9; j++){
+                    if (tab_2[i][j].classList.contains("selected")){
+                        let count = 0
+                        if (j<tab_2.length-1 && (tab_2[i][j+1].classList.contains("selected") || tab_2[i][j+1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if (i<tab_2.length-1 && (tab_2[i+1][j].classList.contains("selected") || tab_2[i+1][j].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if ((j<tab_2.length-1 && i<tab_2.length-1) && (tab_2[i+1][j+1].classList.contains("selected") || tab_2[i+1][j+1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if (j>0 && (tab_2[i][j-1].classList.contains("selected") || tab_2[i][j-1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if (i>0 && (tab_2[i-1][j].classList.contains("selected") || tab_2[i-1][j].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if ((j>0 && i>0) && (tab_2[i-1][j-1].classList.contains("selected") || tab_2[i-1][j-1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if ((j<tab_2.length-1 && i>0) && (tab_2[i-1][j+1].classList.contains("selected") || tab_2[i-1][j+1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if ((i<tab_2.length-1 && j>0) && (tab_2[i+1][j-1].classList.contains("selected") || tab_2[i+1][j-1].classList.contains("fixed"))){
+                            count++ 
+                        }
+                        if (count>=3){
+                            return false
+                        }
+                    }
+                    count = 0
+                }
+            }
+            return true
         }
         console.log(row_count)
     </script>
