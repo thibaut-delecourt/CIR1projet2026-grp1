c8c1e43f2cc87f3b83981b819b8b782e63795707
mignauxleclerteva <eva.mignaux-leclert@student.junia.com>
2026-05-22 10:30:49 +0200

    niveau 2 OK

--- aventure_2.php
+++ aventure_2.php
@@ -456,8 +456,8 @@
     <script>
         const grid = document.getElementById("grid");
 
-        const rowNumbers = [1, 6, 1, 4, 3, 4, 3, 4, 7];
-        const colNumbers = [6, 4, 4, 3, 3, 7, 1, 4, 1];
+        const rowNumbers = [1, 2, 2, 2, 2, 2, 2, 2, 2];
+        const colNumbers = [2, 2, 2, 2, 2, 2, 2, 2, 1];
 
         let victoireDejaAffichee = false;
         let countdownInterval = null;
