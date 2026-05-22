3f295dd8e349db887daaa5259d1186b250ea3c5e
mignauxleclerteva <eva.mignaux-leclert@student.junia.com>
2026-05-22 09:52:18 +0200

    code remis

--- aventure_1.php
+++ aventure_1.php
@@ -452,8 +452,8 @@
     <script>
         const grid = document.getElementById("grid");
 
-        const rowNumbers = [1, 6, 1, 4, 3, 4, 3, 4, 7];
-        const colNumbers = [6, 4, 4, 3, 3, 7, 1, 4, 1];
+        const rowNumbers = [1,1,1,1,1,1,1,1,9];
+        const colNumbers = [9,1,1,1,1,1,1,1,1];
 
         let victoireDejaAffichee = false;
         let countdownInterval = null;
